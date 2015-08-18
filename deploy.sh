c4b7a35234b6ae0d099bb2eb35172695449bfbfd
evatlsong <evatlsong@gmail.com>
2015-08-18 23:29:59 +0800

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
+node_modules/.bin/md2resume resume.md 
 if [ -d ./.deploy_git/ ]
 then
     mv resume.html .deploy_git/
+    cd .deploy_git/
     git add .
     git commit -m 'update'
     echo "commit"
 else
     mkdir -p .deploy_git/
     mv resume.html .deploy_git/
+    cd .deploy_git/
+    git init
     git add .
     git commit -m 'update'
     echo "create && add"
